e74da589139890fc3426fa948146d61487520780
Ratchapol Chantarachote <me@ratchapol.ch>
2025-11-03 10:06:28 +0700

    update so any file extensions are welcome, and handle some special case.

--- main.py
+++ main.py
@@ -1,16 +1,28 @@
-import pandas as pd
+import chardet
 import json
-import tkinter as tk
 import os
+import pandas as pd
+import tkinter as tk
 
 from tkinter import filedialog
+
+
+def detect_encoding(file_path: str):
+    with open(file_path, "rb") as file:
+        detector = chardet.universaldetector.UniversalDetector()
+        for line in file:
+            detector.feed(line)
+            if detector.done:
+                break
+        detector.close()
+    return detector.result["encoding"]
 
 
 def get_file_path():
     file_path = filedialog.askopenfilename(
         initialdir=os.path.abspath(__file__),
         title="Select a File",
-        filetypes=[("JSON", "*.json")],
+        filetypes=[("JSON", "*.json"), ("JSON in TXT", "*.txt"), ("Other", "*")],
     )
 
     return file_path
@@ -32,8 +44,20 @@ if __name__ == "__main__":
 
         sys.exit()
 
-    with open(file_path, "rt", encoding="utf-8") as f:
-        json_data = json.load(f)
+    encoding = detect_encoding(file_path)
+    encoding = "utf-8" if encoding is None else encoding
+
+    with open(file_path, "rt", encoding=encoding) as f:
+        try:
+            json_data = json.load(f)
+        except json.decoder.JSONDecodeError as e:
+            f.seek(0)
+            json_raw_string = f.read()
+            json_raw_string = json_raw_string.replace("﻿", ",")
+            if json_raw_string[0] != "[" or json_raw_string[-1] != "]":
+                json_raw_string = f"[{json_raw_string.lstrip('[').rstrip(']')}]"
+            
+            json_data = json.loads(json_raw_string)
 
     df = pd.DataFrame(json_data)
     df.to_excel(f"{file_name}.xlsx", index=False)
