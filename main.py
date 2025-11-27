4d3249d1ea8dd599e16763b53097dca7eb2fc211
Ratchapol Chantarachote <ratchapol_c@katsan.co.th>
2025-11-27 16:52:44 +0700

    fix bugs

--- main.py
+++ main.py
@@ -1,13 +1,30 @@
-import chardet
+"""Main file"""
+
+# pylint: disable=W0621, C0103
+
+# | IMPORT
 import json
 import os
-import pandas as pd
 import tkinter as tk
 
 from tkinter import filedialog
 
+import chardet
+import pandas as pd
 
-def detect_encoding(file_path: str):
+
+# | FUNCTIONS
+
+
+def detect_encoding(file_path: str) -> str | None:
+    """Detect file encodiung
+
+    Args:
+        file_path (str): file path
+
+    Returns:
+        str | None: file encoding
+    """
     with open(file_path, "rb") as file:
         detector = chardet.universaldetector.UniversalDetector()
         for line in file:
@@ -18,7 +35,12 @@ def detect_encoding(file_path: str):
     return detector.result["encoding"]
 
 
-def get_file_path():
+def get_file_path() -> str:
+    """Get file path from file select windows
+
+    Returns:
+        str: file path
+    """
     file_path = filedialog.askopenfilename(
         initialdir=os.path.abspath(__file__),
         title="Select a File",
@@ -28,6 +50,8 @@ def get_file_path():
     return file_path
 
 
+# | MAIN
+
 if __name__ == "__main__":
     root = tk.Tk()
     root.withdraw()
@@ -36,7 +60,7 @@ if __name__ == "__main__":
     file_name = os.path.splitext(os.path.split(file_path)[1])[0]
 
     if not file_path:
-        tk.messagebox.showwarning(
+        tk.messagebox.showwarning(  # type: ignore
             title="No file select!", message="No file select!\nExiting..."
         )
         root.destroy()
@@ -50,18 +74,21 @@ if __name__ == "__main__":
     with open(file_path, "rt", encoding=encoding) as f:
         try:
             json_data = json.load(f)
+            if not isinstance(json_data, list):
+                json_data = [json_data.copy()]
         except json.decoder.JSONDecodeError as e:
             f.seek(0)
             json_raw_string = f.read()
             json_raw_string = json_raw_string.replace("﻿", ",")
+            json_raw_string = json_raw_string.replace(chr(0x00), "")
             if json_raw_string[0] != "[" or json_raw_string[-1] != "]":
                 json_raw_string = f"[{json_raw_string.lstrip('[').rstrip(']')}]"
-            
+
             json_data = json.loads(json_raw_string)
 
     df = pd.DataFrame(json_data)
     df.to_excel(f"{file_name}.xlsx", index=False)
-    tk.messagebox.showinfo(
+    tk.messagebox.showinfo(  # type: ignore
         title="The file has been converted!",
         message="The file has been converted!\nClosing...",
     )
